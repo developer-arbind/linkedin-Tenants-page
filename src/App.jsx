332a7ef5c32a8396f416df7104d62e30b0d918fe
Arbind Kumar <coding.arbind@gmail.com>
2024-02-27 03:34:54 +0545

    updated tenantcatg

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,9 @@ export default class CCTCategory extends React.Component {
       error: {
          occured: false,
         message: ""
-      }
+      },
+      
+      expand: false
     };
     this.filterSupportedFields = this.filterSupportedFields.bind(this);
     this.addSupportedField = this.addSupportedField.bind(this);
@@ -397,11 +399,33 @@ export class SupportedFieldRow extends React.Component {
     return (
       <div className="columns-2">
         <div className="row-title">
-          <h2>{this.props.item.ucfContentExtractionFieldId}</h2>
+        <details id={`${this.props.item.ucfContentExtractionFieldId}`}>
+          <summary style={{display: "inline"}}>{this.props.item.ucfContentExtractionFieldId}</summary>
           <h3>{this.props.item.ucfContentExtractionFieldDes}</h3>
           <h4>{this.props.item.createdOn}</h4>
-          <button onClick={this.onOpenModal}>edit</button>
-          <button>expand</button>
+         
+        </details>
+        <p style={{
+            color: "black",
+            cursor: "pointer",
+            display: "inline",
+            float: "right"
+          }} onClick={() => {
+            if(!this.state.expand){ 
+               document.getElementById(this.props.item.ucfContentExtractionFieldId).setAttribute("open", "true");
+            }else {
+              document.getElementById(this.props.item.ucfContentExtractionFieldId).removeAttribute("open");
+            }
+            this.setState({expand: !this.state.expand ? true : false});
+          }}>{!this.state.expand ?  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="10" height="10" viewBox="0 0 50 50">
+          <path d="M 44.988281 13.984375 C 44.726563 13.992188 44.476563 14.101563 44.292969 14.292969 L 25 33.585938 L 5.707031 14.292969 C 5.519531 14.097656 5.261719 13.992188 4.992188 13.988281 C 4.582031 13.992188 4.21875 14.238281 4.0625 14.613281 C 3.910156 14.992188 4 15.421875 4.292969 15.707031 L 24.292969 35.707031 C 24.683594 36.097656 25.316406 36.097656 25.707031 35.707031 L 45.707031 15.707031 C 46.003906 15.421875 46.09375 14.980469 45.9375 14.601563 C 45.777344 14.222656 45.402344 13.976563 44.988281 13.984375 Z"></path>
+          </svg> :<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="10" height="10" viewBox="0 0 32 32">
+<path d="M29.71,21.79l-1.42,1.42L16,10.91,3.71,23.21,2.29,21.79,16,8.09Z"></path>
+</svg>}</p>
+        <p style={{color: "black", cursor: "pointer", display: "inline", float: "right"}} onClick={this.onOpenModal}><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="10" height="10" viewBox="0 0 50 50">
+<path d="M 46.574219 3.425781 C 45.625 2.476563 44.378906 2 43.132813 2 C 41.886719 2 40.640625 2.476563 39.691406 3.425781 C 39.691406 3.425781 39.621094 3.492188 39.53125 3.585938 C 39.523438 3.59375 39.511719 3.597656 39.503906 3.605469 L 4.300781 38.804688 C 4.179688 38.929688 4.089844 39.082031 4.042969 39.253906 L 2.035156 46.742188 C 1.941406 47.085938 2.039063 47.453125 2.292969 47.707031 C 2.484375 47.898438 2.738281 48 3 48 C 3.085938 48 3.171875 47.988281 3.257813 47.964844 L 10.746094 45.957031 C 10.917969 45.910156 11.070313 45.820313 11.195313 45.695313 L 46.394531 10.5 C 46.40625 10.488281 46.410156 10.472656 46.417969 10.460938 C 46.507813 10.371094 46.570313 10.308594 46.570313 10.308594 C 48.476563 8.40625 48.476563 5.324219 46.574219 3.425781 Z M 45.160156 4.839844 C 46.277344 5.957031 46.277344 7.777344 45.160156 8.894531 C 44.828125 9.222656 44.546875 9.507813 44.304688 9.75 L 40.25 5.695313 C 40.710938 5.234375 41.105469 4.839844 41.105469 4.839844 C 41.644531 4.296875 42.367188 4 43.132813 4 C 43.898438 4 44.617188 4.300781 45.160156 4.839844 Z M 5.605469 41.152344 L 8.847656 44.394531 L 4.414063 45.585938 Z"></path>
+</svg></p>
+         
           {this.state.open && (
             RenderPopUP(
               true,
